72647e2d5b2f54c941df697ec7d2e81e6ce974ab
Vano <simplmd@gmail.com>
2025-07-01 01:14:24 +0700

    up

--- source/Main.hx
+++ source/Main.hx
@@ -14,10 +14,19 @@ import away3d.materials.methods.BasicDiffuseMethod;
 import away3d.materials.methods.BasicSpecularMethod;
 import away3d.textures.BitmapTexture;
 import openfl.Assets;
+import openfl.geom.Vector3D;
+import away3d.primitives.LineSegment;
+import away3d.entities.SegmentSet;
 
 class Main extends Sprite
 {
 	var view3D:View3D;
+	var sphere:Mesh;
+	var chain:SegmentSet;
+	var anchor:Vector3D;
+	var chainLength:Int = 8;
+	var chainSegments:Array<LineSegment>;
+	var time:Float = 0;
 
 	public function new()
 	{
@@ -25,7 +34,6 @@ class Main extends Sprite
 
 		view3D = new View3D();
 		addChild(view3D);
-
 
 		var light = new PointLight();
 		light.x = 300;
@@ -40,16 +48,51 @@ class Main extends Sprite
 		material.specular = 1;
 		material.gloss = 50;
 
-		var sphere = new Mesh(new SphereGeometry(100, 40, 32), material); 
+
+		anchor = new Vector3D(0, -200, 0);
+
+
+		sphere = new Mesh(new SphereGeometry(40, 40, 32), material);
 		view3D.scene.addChild(sphere);
 
+		// Цепь
+		chain = new SegmentSet();
+		chainSegments = [];
+		for (i in 0...chainLength) {
+			var seg = new LineSegment(anchor, anchor, 0x888888, 0x888888, 3);
+			chain.addSegment(seg);
+			chainSegments.push(seg);
+		}
+		view3D.scene.addChild(chain);
 
 		// var flxGame = new FlxGame(0, 0, PlayState);
 		// addChild(flxGame);
 
-
 		this.addEventListener("enterFrame", function(_) {
-			sphere.rotationY += 1;
+			time += 0.02;
+	
+			var radius = 120.0;
+			var angle = Math.sin(time) * 0.7; 
+			var x = Math.sin(angle) * radius;
+			var y = Math.cos(angle) * radius;
+			var z = Math.cos(time) * 40;
+			sphere.x = x;
+			sphere.y = y;
+			sphere.z = z;
+
+
+			var prev = anchor;
+			for (i in 0...chainLength) {
+				var t = (i + 1) / chainLength;
+				var cx = anchor.x + (sphere.x - anchor.x) * t;
+				var cy = anchor.y + (sphere.y - anchor.y) * t;
+				var cz = anchor.z + (sphere.z - anchor.z) * t;
+				var next = new Vector3D(cx, cy, cz);
+				chainSegments[i].start = prev;
+				chainSegments[i].end = next;
+				prev = next;
+			}
+
 			view3D.render();
 		});
 	}
